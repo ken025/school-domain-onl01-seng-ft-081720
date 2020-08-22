b54f8a7789e023cdbba160b7ca02737d34f5630e
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-22 02:39:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,23 +1,23 @@
-# code here!
-class School
-	attr_accessor :roster, :name, :grade 
-	attr_reader :school 
-	
-	def initialize(school)
-    @school = school
-  end
-  
+
   def roster
-  	roster = {}
-  end 
-  
-  def add_student(name,grade)
-  	roster[grade] = []
-  	roster[grade] << name
-  		
+    @roster
   end
 
-end
+  def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
 
+  def grade(grade)
+    @roster[grade]
+  end
 
-School.new("R").roster.add_student("Zach Morris",9)
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+end 
